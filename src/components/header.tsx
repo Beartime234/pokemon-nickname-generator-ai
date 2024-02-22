3ba1850e6fc0d2f0893410242b286e745c9e9606
Beartime234 <josheatonis234@gmail.com>
2024-02-21 21:40:32 -0800

    Add logo to main site

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import React from "react"
+import Image from "next/image"
 
 // No prop is used in the component; however, kept BodyProps for future scalability.
 type HeaderProps = {}
@@ -8,17 +9,19 @@ type HeaderProps = {}
 export const Header = (props: HeaderProps) => {
     return (
         <>
-            <a href={"/"}>
-                <h1
-                    className="animate-fade-up bg-gradient-to-br from-foreground to-secondary bg-clip-text text-center font-display text-3xl font-bold tracking-[-0.02em] text-transparent drop-shadow-sm [text-wrap:balance] md:text-6xl md:leading-[5rem]"
-                    style={{
-                        animationDelay: "0.15s",
-                        animationFillMode: "forwards",
-                    }}
-                >
-                    Pokémon Nickname AI
-                </h1>
-            </a>
+            <div>
+                <a href={"/"} className="flex justify-center">
+                    <h1
+                        className="animate-fade-up bg-gradient-to-br from-foreground to-secondary bg-clip-text text-center font-display text-3xl font-bold tracking-[-0.02em] text-transparent drop-shadow-sm [text-wrap:balance] md:text-6xl md:leading-[5rem] mt-2"
+                        style={{
+                            animationDelay: "0.15s",
+                            animationFillMode: "forwards",
+                        }}
+                    >
+                        Pokémon Nickname AI
+                    </h1>
+                </a>
+            </div>
             <p
                 className="mt-6 animate-fade-up text-center text-secondary-foreground [text-wrap:balance] md:text-xl pb-4"
                 style={{
--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -12,10 +12,21 @@ import {
 } from "@/components/ui/navigation-menu"
 import { ModeToggle } from "@/components/mode-toggle"
 import { Previous } from "@/components/previous"
+import Image from "next/image"
 export function Menu() {
     return (
         <NavigationMenu>
             <NavigationMenuList>
+                <NavigationMenuItem>
+                    <a href={"/"} className={"flex justify-center"}>
+                        <Image
+                            src={"/images/logo-bgremove.png"}
+                            alt={"logo"}
+                            height={35}
+                            width={35}
+                        />
+                    </a>
+                </NavigationMenuItem>
                 <NavigationMenuItem>
                     <Previous />
                 </NavigationMenuItem>

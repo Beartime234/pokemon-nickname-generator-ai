a8fb68e8bc666c259a73ba81065beec71fe10c82
Beartime234 <josheatonis234@gmail.com>
2024-05-13 11:40:40 +1200

    Update Model to generic gpt-4-turbo

--- api/index.py
+++ api/index.py
@@ -58,7 +58,7 @@ def generate_nickname(
     )
     print(assistant_prompt)
     open_ai_response = client.chat.completions.create(
-        model="gpt-4-0125-preview",
+        model="gpt-4-turbo",
         response_format={"type": "json_object"},
         temperature=temperature,
         messages=[
